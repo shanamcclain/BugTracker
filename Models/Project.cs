289b64a3d9d2fd6524130c7b96ce9231b5cfbf00
shanamcclain <35242747+shanamcclain@users.noreply.github.com>
2018-03-27 16:20:31 -0400

    update

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace BugTracker.Models
 {
@@ -8,6 +9,7 @@ namespace BugTracker.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string PMID { get; set; }
+        [AllowHtml]
         public string Description { get; set; }
 
         //navigation property
--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BugTracker.Models
 {
@@ -9,6 +10,7 @@ namespace BugTracker.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        [AllowHtml]
         public string Description { get; set; }
         public DateTime Created { get; set; }
         public DateTime? Updated { get; set; }
